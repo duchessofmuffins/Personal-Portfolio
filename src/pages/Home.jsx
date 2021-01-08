c0d2a9891e7e4b35b3882d7b8b50aa71f9707eb7
Alice Golter <inkflight@gmail.com>
2021-01-07 20:04:40 -0600

    adding code to Projects, and edited Home page paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ class Home extends Component {
         <div className="container">
           <div className="col-8">
             <h2>Hello!</h2>
-              <p>My name is Alice Golter and I am a 25 years old artist, musician, and entry level 
-                developer living in Minneapolis, MN, USA. I graduated in 2019 from the University of St. Thomas
+              <p>My name is Alice Golter and I am an artist, musician, and entry level 
+                developer living in Minneapolis, MN, USA. I graduated from the University of St. Thomas
                 in Saint Paul, MN with a Bachelor's Degree in Music, Magnum Cum Laude, and got my Certification
                 as an Entry Level Full Stack Developer from the University of Minnesota in 2020. I currently live
                 in Minneapolis with my partner and three pets: two cats Duchess and Muffin, and our dog Mabel.</p>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,3 +3,31 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Jumbotron from '../components/Jumbotron.jsx';
 import './Projects.css';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Jumbotron title="Projects" />
+          <div className="container">
+            <div className="row projects">
+              <div className="row one">
+                <div className="col-xs-2">
+                    <h3>Project Name/Image</h3>
+                </div>
+                <div className="col-xs-2">
+                    <h3>Github Link</h3>
+                </div>
+                <div className="col-xs-2">
+                    <h3>Deployed Project</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        );
+    }
+}
+
+export default Projects
